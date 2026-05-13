0c14079fd200fa480da7753a8839dd3c8dc94f74
studygianella-netizen <studygianella@gmail.com>
2026-05-12 20:10:03 -0500

    Enhance app with images and pricing logic
    Added image display and price calculation for session 2.

--- app.py
+++ app.py
@@ -1,21 +1,33 @@
-import streamlit as st 
+import streamlit as st
 
 st.title("Mi primera aplicación en python")
 
 st.sidebar.title("Parámetros")
 
-st.write("Elaborado por Gianella Z.")
+st.write("Elaborado por: Gianella Z")
 
-Sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
+st.sidebar.image("Image20260512194119.png")
 
-if Sesion == "Sesión 1":
-    st.write("Bienvenido la sesión 1")
-    
-elif  Sesion == "Sesión 2" : 
-    st.write("Bienvenido la sesión 2")
-    
-elif  Sesion == "Sesión 3" : 
-    st.write("Bienvenido la sesión 3")
-    
-else
-    st.write("Bienvenido la sesión 4")
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
+
+if sesion == "Sesión 1":
+  st.write("Bienvenido la sesión 1")
+  st.image("Image20260512194059.png" )
+
+elif sesion == "Sesión 2":
+  st.write("Bienvenido la sesión 2")
+
+  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
+  descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
+
+  precio_final_producto = precio - (precio*(descuento/100))
+
+  st.write("El precio final del producto es: ", precio_final_producto  )
+
+
+
+elif sesion == "Sesión 3":
+  st.write("Bienvenido la sesión 3")
+
+else:
+  st.write("Bienvenido la sesión 4")
