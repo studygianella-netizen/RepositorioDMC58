c9740b51364cbf666ea8d7f39c984cabe75c4700
studygianella-netizen <studygianella@gmail.com>
2026-05-12 19:49:19 -0500

    Move selectbox to sidebar for better layout

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Gianella Z.")
 
-Sesion = st.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
+Sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
