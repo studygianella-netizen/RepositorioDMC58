5d5dc59926fb8b57e3474554fe3c2d77db60625f
studygianella-netizen <studygianella@gmail.com>
2026-05-12 19:47:56 -0500

    Add session selection to Streamlit app

--- app.py
+++ app.py
@@ -5,3 +5,5 @@ st.title("Mi primera aplicación en python")
 st.sidebar.title("Parámetros")
 
 st.write("Elaborado por Gianella Z.")
+
+Sesion = st.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"])
