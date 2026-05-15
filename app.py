408d08dd66be46d994480481113df55dad832976
studygianella-netizen <studygianella@gmail.com>
2026-05-14 21:09:16 -0500

    Enhance session 3 with a value slider
    Added a slider for selecting a value in session 3.

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ elif sesion == "Sesión 2":
 
 elif sesion == "Sesión 3":
   st.write("Bienvenido la sesión 3")
-
+  
+  inicio_rango = st.slider ("Seleccione un valor",min_value = 0 , max_value = 20, value = 7)
 else:
   st.write("Bienvenido la sesión 4")
