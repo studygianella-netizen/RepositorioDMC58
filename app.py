4f8dc7ae9558dd290f2e1e5cff96c64e4d5722ae
studygianella-netizen <studygianella@gmail.com>
2026-05-14 21:45:34 -0500

    Refactor app.py for improved readability and logic

--- app.py
+++ app.py
@@ -41,11 +41,11 @@ elif sesion == "Sesión 3":
     st.write(arreglo)
 
 else:
-  st.write("Bienvenido la sesión 4")
-  principal=st.number_input("Ingrese el monto del prestado", value=1000)
-  tasa_anual=st.number_input("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0, max_value=1.0)
-  anos=st.number_input("Ingrese el npumero de años del préstamo", value=1)
-  pagos_anio = st.number_input("Ingrese la cantidad de pagos por año", value=12)
-
-  cuota = lf.cuota_prestamo(principal,tasa_anual,años,pagos_año)
-  st.write(f"El valor de la cuota es {cuota}")
+   st.write("Bienvenido a la sesión 4")
+   principal = st.number_input("Ingrese el monto del préstamo", value=1000)
+   tasa_anual = st.number_input("Ingrese la tasa anual en decimal", value=0.1, min_value=0.0, max_value=1.0)
+   anios = st.number_input("Ingrese el número de años del préstamo", value=1)
+   pagos_anio = st.number_input("Ingrese la cantidad de pagos por año", value=12)
+    
+   cuota = round(lf.cuota_prestamo(principal, tasa_anual, anios, pagos_anio),2)
+   st.write(f"El valor de la cuota es {cuota}")
