ba3f40cec12b9b1714dabcfb862f0c164bb905ee
studygianella-netizen <studygianella@gmail.com>
2026-05-14 21:17:30 -0500

    Refactor app.py to include numpy import

--- app.py
+++ app.py
@@ -26,13 +26,20 @@ elif sesion == "Sesión 2":
 
 
 elif sesion == "Sesión 3":
-  st.write("Bienvenido la sesión 3")
-  
-  fin_rango = st.slider("Selecione un valor",min_value = 0 , max_value=20, value =7 )
+    import numpy as np
 
-  arreglo = np.arange(0 , fin_rango)
+    st.write("Bienvenido la sesión 3")
 
-  st.write(arreglo)
+    fin_rango = st.slider(
+        "Seleccione un valor",
+        min_value=0,
+        max_value=20,
+        value=7
+    )
+
+    arreglo = np.arange(0, fin_rango)
+
+    st.write(arreglo)
 
 else:
   st.write("Bienvenido la sesión 4")
