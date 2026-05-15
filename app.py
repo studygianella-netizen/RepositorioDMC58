b839a891ef4939545370e6d98e25742e9837da38
studygianella-netizen <studygianella@gmail.com>
2026-05-14 21:13:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,7 @@ elif sesion == "Sesión 2":
 
 elif sesion == "Sesión 3":
   st.write("Bienvenido la sesión 3")
+  import numpy as np
   
   fin_rango = st.slider ("Seleccione un valor",min_value = 0 , max_value = 20, value = 7)
   arreglo= np.arange (0, fin_rango)
